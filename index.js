501500346882e254b0fa02d49acafcb70f3f6daa
garrettprince <garrettjamesprince@gmail.com>
2022-12-12 10:40:18 -0800

    ffmpeg install and bug fixes in index

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const { token } = require("./config.json");
 
 const LOAD_SLASH = process.argv[2] == "load";
 
-const CLIENT_ID = "";
-const GUILD_ID = "";
+const CLIENT_ID = "894031093035073546";
+const GUILD_ID = "915711521525469266";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
@@ -29,16 +29,16 @@ client.player = new Player(client, {
 let commands = [];
 
 const slashFiles = fs
-  .readdirSync("./slash")
+  .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
 for (const file of slashFiles) {
-  const slashcmd = require(`./slash/${file}`);
+  const slashcmd = require(`./commands/${file}`);
   client.slashcommands.set(slashcmd.data.name, slashcmd);
   if (LOAD_SLASH) commands.push(slashcmd.data.toJSON());
 }
 
 if (LOAD_SLASH) {
-  const rest = new REST({ version: "9" }).setToken(TOKEN);
+  const rest = new REST({ version: "9" }).setToken(token);
   console.log("Deploying slash commands");
   rest
     .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
